a8a83eabe3e158eb164885c6c0e7b7007731a758
Utkarsh <r.utkarsh.0010@gmail.com>
2022-04-04 03:26:39 +0530

    React-redux example

---
+++ src/components/CakeContainer.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { buyCake } from '../redux'
+
+function CakeContainer(props) {
+  return (
+    <div>
+        <h2>number of cakes - {props.total_cakes} </h2>
+        <button onClick={props.buyCake}>Buy Cake</button>
+    </div>
+  )
+}
+
+const mapStateToProps = state => {
+    return {
+        total_cakes : state.total_cakes,
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        buyCake : () => {
+            dispatch(buyCake())
+        }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CakeContainer)
---
+++ src/redux/cake/cakeReducer.js
@@ -0,0 +1,19 @@
+import { BUY_CAKE } from "./cakeTypes"
+
+const inital_state = {
+    total_cakes : 10
+}
+
+const cakeReducer = (state = inital_state, action) =>{
+    switch(action.type){
+        case BUY_CAKE:
+            return{
+                ...state, 
+                total_cakes : state.total_cakes - 1
+            }
+
+        default: return state
+    }
+}
+
+export default cakeReducer
